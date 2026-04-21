00f48c488c14608bc6decac5f528dbb30038735b
Diana <dianamorenoarranz@gmail.com>
2026-04-22 01:01:27 +0200

    feat(pages): add ProductDetails page

---
+++ src/pages/ProductsListPage.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../widgets/ui/Header/Header';
+import { ProductGrid } from '../widgets/ui/ProductGrid';
+
+export const ProductsListPage: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <ProductGrid />
+    </>
+  );
+};
