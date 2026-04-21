a6d2fa86a6ad3bb60594603ff7508d4e10b13c79
Diana <dianamorenoarranz@gmail.com>
2026-04-21 12:03:06 +0200

    feat(app): edit app

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
+import { ProductsList } from '../pages/ProductsList';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<p>Home (pendiente)</p>} />
+      <Route path="/" element={<ProductsList />} />
       <Route path="/products/:id" element={<p>Detalle (pendiente)</p>} />
       <Route path="/cart" element={<p>Carrito (pendiente)</p>} />
       <Route path="*" element={<p>404</p>} />
