881ad3154b663900f474b2ad4dc4ad0ebfce748d
Diana <dianamorenoarranz@gmail.com>
2026-04-22 01:03:06 +0200

    refac(routes): rename

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import { ProductsList } from '../pages/ProductsList';
+import { ProductsListPage } from '../pages/ProductsListPage';
+import { ProductDetailPage } from '../pages/ProductDetailsPage';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<ProductsList />} />
-      <Route path="/products/:id" element={<p>Detalle (pendiente)</p>} />
+      <Route path="/" element={<ProductsListPage />} />
+      <Route path="/products/:id" element={<ProductDetailPage />} />
       <Route path="/cart" element={<p>Carrito (pendiente)</p>} />
       <Route path="*" element={<p>404</p>} />
     </Routes>
