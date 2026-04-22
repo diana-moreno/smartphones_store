3bc7d27bfe8cf00b421d7372a1565f6c520bb9f5
Diana <dianamorenoarranz@gmail.com>
2026-04-22 21:20:44 +0200

    chore(vite): configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      generateScopedName: (name, filename) => {
+        const component = filename
+          .split('/')
+          .pop()!
+          .replace('.module.scss', '');
+        return `${component}__${name}`;
+      },
+    },
+  },
 });
