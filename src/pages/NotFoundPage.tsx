4ab150517b8aec1b56f4e2837dcdf993987fb7f7
Diana <dianamorenoarranz@gmail.com>
2026-04-24 14:43:13 +0200

    feat(notFound): add not found page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import styles from './NotFoundPage.module.scss';
+
+export const NotFoundPage: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <p>Page not found</p>
+    </div>
+  );
+};
